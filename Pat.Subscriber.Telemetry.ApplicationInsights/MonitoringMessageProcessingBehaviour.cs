082c1a8fca083165e38144d97e74b16c4f7d7620
Dave Thompson <david@aurionix.com>
2018-11-26 16:20:22 +0000

    Finally deduced purpose of Success property

--- Pat.Subscriber.Telemetry.ApplicationInsights/MonitoringMessageProcessingBehaviour.cs
+++ Pat.Subscriber.Telemetry.ApplicationInsights/MonitoringMessageProcessingBehaviour.cs
@@ -26,21 +26,22 @@ namespace Pat.Subscriber.Telemetry.ApplicationInsights
                 try
                 {
                     await next(messageContext);
-                    Enrich(operation.Telemetry, messageContext);
+                    MarkSuccessful(operation.Telemetry);
                 }
-                catch (Exception exception)
+                catch (Exception)
                 {
-                    // It's the logger's responsibility to record the exception details.
-                    Enrich(operation.Telemetry, messageContext, exception);
-                    telemetryClient.StopOperation(operation);
+                    // It's the logger's responsibility to record the exception details, this module simply records the message request
+                    MarkAsFailed(operation.Telemetry);
                     throw;
                 }
-
-                telemetryClient.StopOperation(operation);
+                finally
+                {
+                    Enrich(operation.Telemetry, messageContext);
+                }
             }
         }
 
-        private void Enrich(RequestTelemetry telemetry, MessageContext messageContext, Exception exception = null)
+        private static void Enrich(RequestTelemetry telemetry, MessageContext messageContext)
         {
             var enqueuedTimeUtc = messageContext.Message.SystemProperties.EnqueuedTimeUtc;
             var timeFromQueueToCompletion = telemetry.Timestamp + telemetry.Duration - enqueuedTimeUtc;
@@ -48,8 +49,23 @@ namespace Pat.Subscriber.Telemetry.ApplicationInsights
             // Attempt to compensate a little for clock differences
             var timeFromQueueToCompletionSeconds = Math.Max(timeFromQueueToCompletion.TotalSeconds, telemetry.Duration.TotalSeconds);
 
-            telemetry.Success = exception == null ? (bool?)true : null;
             telemetry.Metrics.Add("QueueToCompletionTimeSeconds", timeFromQueueToCompletionSeconds);
+        }
+
+        private static void MarkSuccessful(RequestTelemetry telemetry)
+        {
+            telemetry.Success = true;
+            telemetry.ResponseCode = "200";
+        }
+
+        private static void MarkAsFailed(RequestTelemetry telemetry)
+        {
+            // If Success is set to false, App Insights assumes that the application did not "successfully handle the request" and so does not log it in the requests collection
+            // but will instead only log to the exceptions collection.  We want both the Request and the exception logged (if relevant) so we indicate that every message was
+            // successfully handled by the application.  App Insights seems to expect this in the sense that success means that any error has been caught and processed by
+            // logging and then the application has continued as expected on to the next message.
+            telemetry.Success = true;
+            telemetry.ResponseCode = "500";
         }
     }
 }
